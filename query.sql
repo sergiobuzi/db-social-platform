977a14375a2361a0248312df55e98e1bb0c606ec
SergioBuzi <sergio.buzi96@gmail.com>
2024-05-05 23:42:35 +0200

    :)

--- query.sql
+++ query.sql
@@ -7,21 +7,26 @@ FROM users;
 
 -- 2. Seleziona tutti i post senza Like (13)
 SELECT * 
-FROM posts 
-WHERE (SELECT count(post_id) FROM likes where likes.post_id = posts.id) = 0;
-
-
+FROM likes 
+RIGHT JOIN posts 
+ON likes.post_id=posts.id 
+WHERE likes.post_id IS null;
 
 
 
 -- 3. Conta il numero di like per ogni post (165)
-
+SELECT post_id, COUNT(*) AS num_likes
+FROM likes
+GROUP BY post_id;
 
 
 
 
 -- 4. Ordina gli utenti per il numero di media caricati (25)
-
+SELECT user_id, COUNT(*) AS num_posts
+FROM posts
+GROUP BY user_id
+ORDER BY num_posts;
 
 
 
@@ -33,7 +38,10 @@ WHERE (SELECT count(post_id) FROM likes where likes.post_id = posts.id) = 0;
 
 
 -- 6. Seleziona tutti i post degli utenti tra i 20 e i 30 anni (49)
-
+SELECT posts.*
+FROM posts
+JOIN users ON posts.user_id = users.id
+WHERE YEAR(CURDATE()) - YEAR(users.birthdate) BETWEEN 20 AND 30;
 
 
 
